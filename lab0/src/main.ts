2ad1b12c48627a72bf2274e454a23ffae42a24c6
Konstantin Volodin <volodin.konstantin@gmail.com>
2025-02-28 19:51:27 +0000

    Изменен проект для использования на неделе 3+

--- lab0/src/main.ts
+++ lab0/src/main.ts
@@ -1,1 +1,25 @@
-console.log("Hello, world");
+let canvas = document.getElementById('myCanvas') as
+                 HTMLCanvasElement;
+
+let context = canvas.getContext("2d");
+
+let lastTime = 0;
+function animate(currentTime: number): void {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    requestAnimationFrame(animate);
+
+    const deltaTime = (currentTime - lastTime) / 1000; // Convert milliseconds to seconds
+    lastTime = currentTime;
+
+    for (let i=0; i< 26*2; i++) {
+        context.fillStyle = "black";
+        context.fillRect(i * 10, 512/2, 5, 5+100*Math.sin(lastTime/1000));
+    }
+
+    console.log(currentTime/1000);
+}
+
+animate(0);
+
+console.log("End of script");
